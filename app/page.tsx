0b7d6bfbda3aaf3c02a695399ad78a82e5e34906
CHINEDU OKEREKE <Chinedu02k@gmail.com>
2024-12-09 18:41:42 +0000

    create login auth files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Hello World
+      <LoginButton>
+        <Button size="lg" >
+          Click me
+        </Button>
+      </LoginButton>
       <Button size="lg" >Click me</Button>
     </div>
   );
---
+++ notes.md
@@ -0,0 +1,4 @@
+[{
+	"resource": "/workspace/adivo/components/auth/login-button.tsx",
+	"message": "'asChild' is defined but never used.",
+}]
