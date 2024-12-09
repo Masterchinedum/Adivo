eb9eea421ab953ca171abaa2f3f6cdee64ce3821
CHINEDU OKEREKE <Chinedu02k@gmail.com>
2024-12-09 19:21:01 +0000

    add the lib and data files

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { Input } from "../ui/input"
-import { Button } from "../ui/button"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
 import { useState, useTransition } from "react"
 import { useSearchParams } from "next/navigation"
