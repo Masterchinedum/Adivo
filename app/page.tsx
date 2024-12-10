8314a0b70a9bde9a17a7a32cb993a17040347ea0
CHINEDU OKEREKE <Chinedu02k@gmail.com>
2024-12-10 22:43:17 +0000

    Here we go again for the 4th time

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button"
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
-export default function Home() {
+export default async function Index() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
+    <>
+      <Hero />
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      </main>
+    </>
+  );
 }
