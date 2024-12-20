6f0e1231879a2bb086484225b618d0d012578552
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-12-20 04:21:30 +0000

    feat: define User interface with essential properties for user management

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  imageUrl: string;
+  clerkUserId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
