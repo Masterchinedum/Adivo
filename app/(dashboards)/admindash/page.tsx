954a02c77626402d6454d51eabf6749898202fd3
CHINEDU OKEREKE <Chinedu02k@gmail.com>
2024-12-24 23:35:55 +0000

    remove questions.ts validation

--- app/(dashboards)/admindash/page.tsx
+++ app/(dashboards)/admindash/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 const page = () => {
   return (
     <div>
-        Hello this is the admin dashboard, it has a button
+        Hello this is the admin dashboard, It has button
         <Button > click me</Button>
     </div>
   )
