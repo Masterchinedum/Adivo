2c1801c8df89241435b008b6441c3b3ddc0eff3f
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-12-25 15:11:16 +0000

    refactor TestEditPage to simplify structure and update Test model with createdBy field and user relation

--- app/(dashboards)/admindash/tests/[id]/page.tsx
+++ app/(dashboards)/admindash/tests/[id]/page.tsx
@@ -1,39 +1,9 @@
-// app/(dashboards)/admindash/tests/[id]/page.tsx
-import { notFound } from 'next/navigation'
-import prisma from '@/lib/prisma'
-import { TestFormHeader } from "../components/TestFormHeader"
-import { TestEditForm } from "./components/TestEditForm"
-import { TestStatusBar } from "./components/TestStatusBar"
-import { TestDeleteDialog } from "./components/TestDeleteDialog"
+import React from 'react'
 
-interface TestEditPageProps {
-  params: {
-    id: string
-  }
-}
-
-export default async function TestEditPage({ params }: TestEditPageProps) {
-  const test = await prisma.test.findUnique({
-    where: { id: params.id }
-  })
-
-  if (!test) {
-    notFound()
-  }
-
+const page = () => {
   return (
-    <div className="container space-y-8 py-8">
-      <TestFormHeader
-        title="Edit Test"
-        description="Update test details and manage settings."
-      />
-      <TestStatusBar test={test} />
-      <div className="mx-auto max-w-2xl">
-        <TestEditForm test={test} />
-      </div>
-      <div className="mx-auto max-w-2xl">
-        <TestDeleteDialog id={test.id} title={test.title} />
-      </div>
-    </div>
+    <div>page</div>
   )
 }
+
+export default page
