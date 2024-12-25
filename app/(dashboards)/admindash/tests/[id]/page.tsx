cc9f53fb599d066369741c1bf8a33a838b827efa
CHINEDU OKEREKE <Chinedu02k@gmail.com>
2024-12-25 08:21:14 +0000

    edit the pages

--- app/(dashboards)/admindash/tests/[id]/page.tsx
+++ app/(dashboards)/admindash/tests/[id]/page.tsx
@@ -1,45 +1,9 @@
-// app/(dashboards)/admindash/tests/[id]/page.tsx
-import { notFound } from "next/navigation"
-import prisma from "@/lib/prisma"
-import { TestHeader } from "../components/TestHeader"
-import { EditTestForm } from "./EditTestForm"
+import React from 'react'
 
-interface TestEditPageProps {
-  params: {
-    id: string
-  }
-}
-
-async function getTest(id: string) {
-  const test = await prisma.test.findUnique({
-    where: {
-      id: id,
-    },
-    include: {
-      user: true, // Include the user to get createdBy information
-    }
-  })
-
-  if (!test) {
-    notFound()
-  }
-
-  return test
-}
-
-export default async function TestEditPage({ params }: TestEditPageProps) {
-  const test = await getTest(params.id)
-
+const update = () => {
   return (
-    <div className="flex flex-col gap-8 p-8">
-      <TestHeader
-        title={`Edit Test: ${test.title}`}
-        description="Update test details."
-        showAddButton={false}
-      />
-      <div className="mx-auto w-full max-w-2xl">
-        <EditTestForm test={test} />
-      </div>
-    </div>
+    <div>update single test page</div>
   )
 }
+
+export default update
--- app/(dashboards)/admindash/tests/page.tsx
+++ app/(dashboards)/admindash/tests/page.tsx
@@ -1,34 +1,9 @@
-// app/(dashboards)/admindash/tests/page.tsx
+import React from 'react'
 
-import { Suspense } from "react"
-import { TestHeader } from "./components/TestHeader"
-import { TestsTable } from "./components/TestsTable"
-import { TestsTableSkeleton } from "./components/TestsTableSkeleton"
-import { TestFilters } from "./components/TestFilters"
-import prisma from "@/lib/prisma"
-
-async function getTests() {
-  const tests = await prisma.test.findMany({
-    orderBy: {
-      createdAt: 'desc'
-    }
-  })
-  return tests
-}
-
-export default async function TestsPage() {
-  const tests = await getTests()
-
+const Test = () => {
   return (
-    <div className="flex flex-col gap-8 p-8">
-      <TestHeader
-        title="Tests"
-        description="Manage your tests and assessments here."
-      />
-      <TestFilters />
-      <Suspense fallback={<TestsTableSkeleton />}>
-        <TestsTable data={tests} />
-      </Suspense>
-    </div>
+    <div>Test list page</div>
   )
 }
+
+export default Test
