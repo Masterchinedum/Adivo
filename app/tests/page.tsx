f24b6ea056a2392526f0b80f4cf19b6ad1b3fe56
Masterchinedu <chineduokereke02@gmail.com>
2024-12-29 22:36:04 +0000

    add TestPage component for displaying individual and list of tests

---
+++ app/tests/page.tsx
@@ -0,0 +1,14 @@
+//@/app/tests/page.tsx
+
+import React from 'react';
+
+const TestPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <p>This is a simple test list page.</p>
+        </div>
+    );
+};
+
+export default TestPage;
