833aac7fbb06b10a9bb2eda81f02ceefc26e5283
Masterchinedu <chineduokereke02@gmail.com>
2025-01-02 19:43:02 +0000

    feat: add ResultsPage component for displaying test attempt results

---
+++ app/(test-taking)/tests/[testId]/attempt/[attemptId]/results/page.tsx
@@ -0,0 +1,14 @@
+
+
+const ResultsPage: React.FC = () => {
+ 
+    return (
+        <div>
+            <h1>Results for Test </h1>
+            <p>Attempt ID: 12356 </p>
+            {/* Add more details and components to display the results */}
+        </div>
+    );
+};
+
+export default ResultsPage;
