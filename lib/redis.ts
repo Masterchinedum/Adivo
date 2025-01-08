6238120cbf1ce04f0310a9f670479c296c384d04
Masterchinedumhelp <chinedu01k@gmail.com>
2025-01-08 22:23:49 +0000

    feat: update Redis configuration and add dependency for Upstash Redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,16 @@
 import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
-  url: 'https://eu2-lucky-goose-31276.upstash.io',
+  url: 'https://delicate-cheetah-43265.upstash.io',
   token: process.env.REDIS_KEY!,
 })
+
+// import { Redis } from '@upstash/redis'
+
+// const redis = new Redis({
+//   url: 'https://delicate-cheetah-43265.upstash.io',
+//   token: ,
+// })
+
+// await redis.set('foo', 'bar');
+// const data = await redis.get('foo');
