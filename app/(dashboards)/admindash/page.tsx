f75cd07debb748c36b249366e29d7e31b074d380
Masterchinedumhelp <chinedu01k@gmail.com>
2025-01-09 08:21:57 +0000

    feat: add AnalyticsDashboard component for visitor analytics and country tracking

--- app/(dashboards)/admindash/page.tsx
+++ app/(dashboards)/admindash/page.tsx
@@ -1,15 +1,76 @@
-//app/(dashboards)/admindash/page.tsx
+import AnalyticsDashboard from '@/components/AnalyticsDashboard'
+import { getDate } from '@/utils'
+import { analytics } from '@/utils/analytics'
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+const Page = async () => {
+  const TRACKING_DAYS = 7
 
-const page = () => {
+  const pageviews = await analytics.retrieveDays('pageview', TRACKING_DAYS)
+
+  const totalPageviews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!
+      }, 0)
+    )
+  }, 0)
+
+  const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1)
+
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+      )
+    }, 0)
+
+  const topCountriesMap = new Map<string, number>()
+
+  for (let i = 0; i < pageviews.length; i++) {
+    const day = pageviews[i]
+    if (!day) continue
+
+    for (let j = 0; j < day.events.length; j++) {
+      const event = day.events[j]
+      if (!event) continue
+
+      const key = Object.keys(event)[0]!
+      const value = Object.values(event)[0]!
+
+      const parsedKey = JSON.parse(key)
+      const country = parsedKey?.country
+
+      if (country) {
+        if (topCountriesMap.has(country)) {
+          const prevValue = topCountriesMap.get(country)!
+          topCountriesMap.set(country, prevValue + value)
+        } else {
+          topCountriesMap.set(country, value)
+        }
+      }
+    }
+  }
+
+  const topCountries = [...topCountriesMap.entries()].sort((a ,b) => {
+    if(a[1] > b[1]) return -1
+    else return 1
+  }).slice(0, 5)
+
   return (
-    <div>
-        Hello this is the admin dashboard, It has button
-        <Button > <Link href="/admindash/tests">Test list page</Link></Button>
+    <div className='min-h-screen w-full py-12 flex justify-center items-center'>
+      <div className='relative w-full max-w-6xl mx-auto text-white'>
+        <AnalyticsDashboard
+          avgVisitorsPerDay={avgVisitorsPerDay}
+          amtVisitorsToday={amtVisitorsToday}
+          timeseriesPageviews={pageviews}
+          topCountries={topCountries}
+        />
+      </div>
     </div>
   )
 }
 
-export default page
+export default Page
